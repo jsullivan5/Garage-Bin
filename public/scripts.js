9b08c2c68ddb4c0a7741762738df9106ddf6260f
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-14 12:37:39 -0600

    Add append new item on POST request

---
+++ public/scripts.js
@@ -0,0 +1,34 @@
+$('#form-submit').click((event) => {
+  event.preventDefault();
+  const name = $('#form-name').val();
+  const reason = $('#form-reason').val();
+  const cleanliness = $('#form-cleanliness').val();
+
+  fetch('/api/v1/items', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      name,
+      reason,
+      cleanliness
+    })
+  })
+  .then(response => response.json())
+  .then((item) => {
+    const sparklingSelect = item.cleanliness === 'Sparkling' ? 'selected' : null;
+    const dustySelect = item.cleanliness === 'Dusty' ? 'selected' : null;
+    const rancidSelect = item.cleanliness === 'Rancid' ? 'selected' : null;
+    
+    $('#garage-door').prepend(`
+      <section class="item-card">
+        <p>${item.name}</p>
+        <p>${item.reason}</p>
+        <select value=${item.cleanliness.toString()}>
+          <option value="Sparkling" ${sparklingSelect}>Sparkling</option>
+          <option value="Dusty" ${dustySelect}>Dusty</option>
+          <option value="Rancid" ${rancidSelect}>Rancid</option>
+        </select>
+      </section>`)
+  })
+  .catch(error => console.log(error))
+})
