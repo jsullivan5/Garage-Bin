da1c93ff353102f6eea309e01a2d7c0b4560c796
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-14 09:29:19 -0600

    Add basic express server setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Garage Bin';
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+module.exports = app;
