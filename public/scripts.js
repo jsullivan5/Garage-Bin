ee2a05d9551d7d0485f0b1e95b309ce154a86314
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-14 12:50:23 -0600

    Add GET items display on page load

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,28 @@
+$(document).ready(() => {
+  fetch('api/v1/items')
+    .then(response => response.json())
+    .then(items => {
+      console.log(items);
+
+      items.forEach(item => {
+        const sparklingSelect = item.cleanliness === 'Sparkling' ? 'selected' : null;
+        const dustySelect = item.cleanliness === 'Dusty' ? 'selected' : null;
+        const rancidSelect = item.cleanliness === 'Rancid' ? 'selected' : null;
+        
+        $('#garage-door').prepend(`
+          <section class="item-card">
+            <p>${item.name}</p>
+            <p>${item.reason}</p>
+            <select value=${item.cleanliness.toString()}>
+              <option value="Sparkling" ${sparklingSelect}>Sparkling</option>
+              <option value="Dusty" ${dustySelect}>Dusty</option>
+              <option value="Rancid" ${rancidSelect}>Rancid</option>
+            </select>
+          </section>`)
+      })
+    })
+});
+
 $('#form-submit').click((event) => {
   event.preventDefault();
   const name = $('#form-name').val();
@@ -18,7 +43,7 @@ $('#form-submit').click((event) => {
     const sparklingSelect = item.cleanliness === 'Sparkling' ? 'selected' : null;
     const dustySelect = item.cleanliness === 'Dusty' ? 'selected' : null;
     const rancidSelect = item.cleanliness === 'Rancid' ? 'selected' : null;
-    
+
     $('#garage-door').prepend(`
       <section class="item-card">
         <p>${item.name}</p>
