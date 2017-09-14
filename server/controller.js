8a679a4a6ae6c3d0410dd6ca4f941dce8fe8a883
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-14 10:59:06 -0600

    Add GET items route

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,17 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
+
+const getItems = (req, res) => {
+  database('items').select('*')
+    .then(items => {
+      res.status(200).json(items);
+    })
+    .catch(error => {
+      res.status(500).json({ error })
+    })
+}
+
+module.exports = {
+  getItems
+}
--- server/router.js
+++ server/router.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
+router.get('/items', controller.getItems)
+
 module.exports = router;
