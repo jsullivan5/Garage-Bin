424b73eff2f46fea46f41bad0bf1afbd298ea011
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-14 13:58:47 -0600

    Add PATCH update to DB on cleanliness change.

--- public/scripts.js
+++ public/scripts.js
@@ -32,6 +32,19 @@ $('#form-submit').click((event) => {
   .catch(error => console.log(error))
 })
 
+$('#garage-door').on('change', '.card-select', (event) => {
+  const newCleanliness = event.target.value;
+  const id = event.target.name;
+
+  fetch(`/api/v1/items/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ cleanliness: newCleanliness })
+  })
+  .then(response => response.json())
+  .catch(error => console.log(error));
+})
+
 function generateItemCard(item) {
   const sparklingSelect = item.cleanliness === 'Sparkling' ? 'selected' : null;
   const dustySelect = item.cleanliness === 'Dusty' ? 'selected' : null;
@@ -41,7 +54,7 @@ function generateItemCard(item) {
     <section class="item-card">
       <p>${item.name}</p>
       <p>${item.reason}</p>
-      <select name=${item.id}>
+      <select class="card-select" name=${item.id}>
         <option value="Sparkling" ${sparklingSelect}>Sparkling</option>
         <option value="Dusty" ${dustySelect}>Dusty</option>
         <option value="Rancid" ${rancidSelect}>Rancid</option>
